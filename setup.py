7def5778d81f053776c8899acbc490398fb4be54
Eric Larson <eric@ionrock.org>
2012-06-21 11:21:41 -0500

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setup_params = dict(
+    name='HTTPCache',
+    version='0.5',
+    author="Eric Larson",
+    author_email="eric@ionrock.org",
+    url="https://bitbucket.org/elarson/httpcache",
+    packages=setuptools.find_packages(),
+    tests_requires=[
+        'cherrypy',
+    ],
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
