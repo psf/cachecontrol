a1aa55403071653ffecb244b7af9eb0a67c52c9d
Eric Larson <eric@ionrock.org>
2013-06-19 18:22:25 -0500

    Add MIT license so it jives with httplib2 and more detailed setup params

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 import setuptools
 
+long_description = open('README.rst').read()
+
 setup_params = dict(
     name='CacheControl',
     version='0.5',
-    author="Eric Larson",
-    author_email="eric@ionrock.org",
-    url="http://github/ionrock/cachecontrol",
+    author='Eric Larson',
+    author_email='eric@ionrock.org',
+    license='MIT',
+    url='https://github/ionrock/cachecontrol',
+    keywords='requests http caching web',
     packages=setuptools.find_packages(),
+    description='httplib2 caching for requests',
+    long_description=long_description,
     install_requires=[
         'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
     ],
 )
 
