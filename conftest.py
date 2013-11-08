5b00691c3c76b88d7f322e62f5445b42652ae6b2
Eric Larson <eric@ionrock.org>
2013-11-07 21:04:20 -0600

    Move tests outside the package.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,42 @@
+from pprint import pformat
+
 import pytest
+
 from webtest.http import StopableWSGIServer
 
-from cachecontrol.tests import SimpleApp
+
+class SimpleApp(object):
+
+    def dispatch(self, env):
+        path = env['PATH_INFO'][1:].split('/')
+        segment = path.pop(0)
+        if segment and hasattr(self, segment):
+            return getattr(self, segment)
+        return None
+
+    def vary_accept(self, env, start_response):
+        headers = [
+            ('Cache-Control', 'max-age=5000'),
+            ('Content-Type', 'text/plain'),
+            ('Vary', 'Accept-Encoding, Accept'),
+        ]
+        start_response('200 OK', headers)
+        return pformat(env)
+
+    def __call__(self, env, start_response):
+        func = self.dispatch(env)
+
+        if func:
+            return func(env, start_response)
+
+        print('default handler')
+
+        headers = [
+            ('Cache-Control', 'max-age=5000'),
+            ('Content-Type', 'text/plain'),
+        ]
+        start_response('200 OK', headers)
+        return pformat(env)
 
 
 @pytest.fixture(scope='session')
