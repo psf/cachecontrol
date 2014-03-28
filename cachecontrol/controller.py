cd44871957f3fab5cab2da52602678607b1d312a
echel0n <justin.tabish@gmail.com>
2014-03-27 17:11:52 -0700

    Moved code before parsing of response is done in cache_response function, makes more sense. Set max-age to 900 instead of 3600

--- cachecontrol/controller.py
+++ cachecontrol/controller.py
@@ -185,6 +185,17 @@ class CacheController(object):
         if resp.status_code not in [200, 203]:
             return
 
+        # If we want to cache sites not setup with cache headers then add the proper headers and keep the response
+        if self.cache_all and getattr(resp.headers, 'cache-control', None) is None:
+            headers = {'Cache-Control': 'public,max-age=%d' % int(900)}
+            resp.headers.update(headers)
+
+            if getattr(resp.headers, 'expires', None) is None:
+                expires = datetime.datetime.utcnow() + datetime.timedelta(days=(1))
+                expires = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
+                headers = {'Expires': expires}
+                resp.headers.update(headers)
+
         cc_req = self.parse_cache_control(request.headers)
         cc = self.parse_cache_control(resp.headers)
 
@@ -199,15 +210,6 @@ class CacheController(object):
         if self.cache_etags and 'etag' in resp.headers:
             self.cache.set(cache_url, resp)
 
-        # If we want to cache sites not setup with cache headers then add the proper headers and keep the response
-        if self.cache_all:
-            expires = datetime.datetime.utcnow() + datetime.timedelta(days=(25 * 365))
-            expires = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
-            headers = {'Cache-Control': 'public,max-age=%d' % int(3600),
-                       'Expires': expires}
-            resp.headers.update(headers)
-            self.cache.set(cache_url, resp)
-
         # Add to the cache if the response headers demand it. If there
         # is no date header then we can't do anything about expiring
         # the cache.
@@ -219,8 +221,7 @@ class CacheController(object):
 
             # If the request can expire, it means we should cache it
             # in the meantime.
-            elif 'expires' in resp.headers:
-                if resp.headers['expires']:
+            elif getattr(resp.headers, 'expires', None) is None:
                     self.cache.set(cache_url, resp)
 
     def update_cached_response(self, request, response):
