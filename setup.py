e011ac98f43ec1d3d6d5488c270e569737113ec9
Eric Larson <eric@ionrock.org>
2014-07-28 12:35:33 -0500

    Specifically adding 3.2-4 to the trove classifiers.

--- setup.py
+++ setup.py
@@ -26,6 +26,9 @@ setup_params = dict(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
