5544616339ce05123c54fd5fba43fbbc79f8839c
Eric Larson <eric@ionrock.org>
2014-09-10 13:22:52 -0500

    [#39] Use our own is_fp_closed method to check whether a response has been read before caching.
    I copied over the is_fp_closed from urllib3 and reversed the ordering of
    the try / except in order to give preference to HTTPResponses before
    real file pointers.

---
+++ tests/test_regressions.py
@@ -0,0 +1,18 @@
+import sys
+import pytest
+
+
+from cachecontrol import CacheControl
+from cachecontrol.caches.file_cache import FileCache
+from requests import Session
+
+
+class Test39(object):
+
+    @pytest.mark.skipif(sys.version_info.major <= 2,
+                        reason='Only run this for python 3.x')
+    def test_file_cache_recognizes_consumed_file_handle(self):
+        s = CacheControl(Session(), FileCache('web_cache'))
+        s.get('http://httpbin.org/cache/60')
+        r = s.get('http://httpbin.org/cache/60')
+        assert r.from_cache
