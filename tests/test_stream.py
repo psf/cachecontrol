50778f3f8962df2ce79dc8fc83e206d5e8bf96fb
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-11-25 18:35:01 -0800

    Add second test

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,16 +7,22 @@ from cachecontrol import CacheControl
 
 
 class TestStream(object):
-    def setup(self):
-        self.sess = CacheControl(requests.Session())
-
     def test_stream_is_cached(self, url):
-        resp_1 = self.sess.get(url + 'stream')
+        sess = CacheControl(requests.Session())
+
+        resp_1 = sess.get(url + 'stream')
         content_1 = resp_1.content
 
-        resp_2 = self.sess.get(url + 'stream')
+        resp_2 = sess.get(url + 'stream')
         content_2 = resp_1.content
 
         assert not resp_1.from_cache
         assert resp_2.from_cache
         assert content_1 == content_2
+
+    def test_stream_is_not_cached_when_content_is_not_read(self, url):
+        sess = CacheControl(requests.Session())
+        sess.get(url + 'stream', stream=True)
+        resp = sess.get(url + 'stream', stream=True)
+
+        assert not resp.from_cache
