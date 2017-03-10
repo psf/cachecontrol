fbc288d89382d172e2ef6353eff61b14a2ab2b5c
elnuno <elnuno98@tutanota.com>
2017-03-10 00:35:18 -0300

    Make wrapper.CacheControl configurable regarding both controller and adapter.

--- cachecontrol/wrapper.py
+++ cachecontrol/wrapper.py
@@ -6,14 +6,18 @@ def CacheControl(sess,
                  cache=None,
                  cache_etags=True,
                  serializer=None,
-                 heuristic=None):
+                 heuristic=None,
+                 controller_class=None,
+                 adapter_class=None):
 
     cache = cache or DictCache()
-    adapter = CacheControlAdapter(
+    adapter_class = adapter_class or CacheControlAdapter
+    adapter = adapter_class(
         cache,
         cache_etags=cache_etags,
         serializer=serializer,
         heuristic=heuristic,
+        controller_class=controller_class
     )
     sess.mount('http://', adapter)
     sess.mount('https://', adapter)
