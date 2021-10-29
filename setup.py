783226652c8914ae76950d117a713c99d96c0526
Eric Larson <eric@ionrock.org>
2021-10-29 11:39:39 -0500

    Ensure the required python version is PEP 440 compliant
    Fixes #261

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup_params = dict(
     install_requires=["requests", "msgpack>=0.5.2"],
     extras_require={"filecache": ["lockfile>=0.9"], "redis": ["redis>=2.10.5"]},
     entry_points={"console_scripts": ["doesitcache = cachecontrol._cmd:main"]},
-    python_requires=">=3.6.*",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
