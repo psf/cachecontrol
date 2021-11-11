6b214390233aa240c8b90e2a844247bb6eb7fdc6
Itamar Turner-Trauring <itamar@itamarst.org>
2021-11-11 12:03:09 -0500

    Sketch of streaming file cache.

--- cachecontrol/caches/file_cache.py
+++ cachecontrol/caches/file_cache.py
@@ -5,15 +5,13 @@
 import hashlib
 import os
 from textwrap import dedent
+from pathlib import Path
+from typing import Optional, Tuple
 
-from ..cache import BaseCache
+from ..cache import BaseCache, CacheInterface
 from ..controller import CacheController
-
-try:
-    FileNotFoundError
-except NameError:
-    # py2.X
-    FileNotFoundError = (IOError, OSError)
+from ..serialize import Serializer
+from ..compat import HTTPResponse
 
 
 def _secure_open_write(filename, fmode):
@@ -57,8 +55,37 @@ def _secure_open_write(filename, fmode):
         raise
 
 
-class FileCache(BaseCache):
+def _get_dir_lock_class(use_dir_lock, lock_class):
+    if use_dir_lock is not None and lock_class is not None:
+        raise ValueError("Cannot use use_dir_lock and lock_class together")
 
+    try:
+        from lockfile import LockFile
+        from lockfile.mkdirlockfile import MkdirLockFile
+    except ImportError:
+        notice = dedent(
+            """
+        NOTE: In order to use the FileCache you must have
+        lockfile installed. You can install it via pip:
+          pip install lockfile
+        """
+        )
+        raise ImportError(notice)
+
+    else:
+        if use_dir_lock:
+            lock_class = MkdirLockFile
+
+        elif lock_class is None:
+            lock_class = LockFile
+    return lock_class
+
+
+class FileCache(BaseCache):
+    """
+    A legacy cache for backwards compatibility.  You should use
+    ``StreamingFileCache`` instead in new code.
+    """
     def __init__(
         self,
         directory,
@@ -68,30 +95,7 @@ class FileCache(BaseCache):
         use_dir_lock=None,
         lock_class=None,
     ):
-
-        if use_dir_lock is not None and lock_class is not None:
-            raise ValueError("Cannot use use_dir_lock and lock_class together")
-
-        try:
-            from lockfile import LockFile
-            from lockfile.mkdirlockfile import MkdirLockFile
-        except ImportError:
-            notice = dedent(
-                """
-            NOTE: In order to use the FileCache you must have
-            lockfile installed. You can install it via pip:
-              pip install lockfile
-            """
-            )
-            raise ImportError(notice)
-
-        else:
-            if use_dir_lock:
-                lock_class = MkdirLockFile
-
-            elif lock_class is None:
-                lock_class = LockFile
-
+        lock_class = _get_dir_lock_class(use_dir_lock, lock_class)
         self.directory = directory
         self.forever = forever
         self.filemode = filemode
@@ -141,10 +145,91 @@ class FileCache(BaseCache):
                 pass
 
 
-def url_to_file_path(url, filecache):
+def url_to_file_path(url, filecache: FileCache):
     """Return the file cache path based on the URL.
 
     This does not ensure the file exists!
     """
+    assert isinstance(filecache, FileCache)
     key = CacheController.cache_url(url)
     return filecache._fn(key)
+
+
+class StreamingFileCache(CacheInterface):
+    """
+    A file-based cache that uses significantly less memory than ``FileCache``.
+    """
+    def __init__(
+        self,
+        directory,
+        forever=False,
+        filemode=0o0600,
+        dirmode=0o0700,
+        use_dir_lock=None,
+        lock_class=None,
+    ):
+        lock_class = _get_dir_lock_class(use_dir_lock, lock_class)
+        self._directory = Path(directory)
+        self._forever = forever
+        self._filemode = filemode
+        self._dirmode = dirmode
+        self._lock_class = lock_class
+
+    def _filenames(self, name: str) -> Tuple[Path, Path]:
+        """Returns (metadata path, body path)."""
+        hashed = hashlib.sha224(name.encode("utf-8")).hexdigest()
+        parts = list(hashed[:5]) + [hashed]
+        base_path = self._directory / Path(*parts)
+        return base_path.with_suffix(".metadata"), base_path.with_suffix(".body")
+
+    def get_as_file(self, key: str, request, serializer: Serializer) -> HTTPResponse:
+        """
+        Return tuple of (encoded-metadata file-like, body file-like).
+        """
+        metadata_path, body_path = self._filenames(key)
+        return serializer.loads_separate(request, metadata_path.read_bytes(), body_path.open("rb"))
+
+    def set_from_file(
+        self,
+        key: str,
+        metadata_value: bytes,
+        body_path: Path,
+        expires: Optional[int] = None,
+    ):
+        """
+        Store a cache entry, with metadata and body being passed in separately,
+        the latter as a path to a file.  The metadata is encoded.  Expiration
+        time is a duration (from the present moment) in seconds.
+        """
+        metadata_path, new_body_path = self._filenames(key)
+
+        # Make sure the directories exist:
+        metadata_path.parent.mkdir(self._dirmode, parents=True, exists_ok=True)
+
+        with self._lock_class(str(metadata_path)):
+            with self._lock_class(str(body_path)):
+                with _secure_open_write(metadata_path.path, self._filemode) as f:
+                    f.write(metadata_value)
+                body_path.rename(new_body_path)
+                body_path.chmod(self._filemode)
+
+    def delete(self, key: str):
+        """
+        Delete an entry.
+        """
+        if not self._forever:
+            metadata_path, new_body_path = self._filenames(key)
+            try:
+                metadata_path.remove()
+            except FileNotFoundError:
+                pass
+            try:
+                new_body_path.remove()
+            except FileNotFoundError:
+                pass
+
+    def close(self):
+        """
+        Close the cache.
+        """
+        pass
