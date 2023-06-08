db710af8f3e177cca1070924b8a0b44e8d78a03b
William Woodruff <william@yossarian.net>
2023-06-08 00:28:40 -0400

    cachecontrol: 0.13.1rc0

--- cachecontrol/__init__.py
+++ cachecontrol/__init__.py
@@ -8,7 +8,7 @@ Make it easy to import from cachecontrol without long namespaces.
 """
 __author__ = "Eric Larson"
 __email__ = "eric@ionrock.org"
-__version__ = "0.13.0"
+__version__ = "0.13.1rc0"
 
 from cachecontrol.adapter import CacheControlAdapter
 from cachecontrol.controller import CacheController
